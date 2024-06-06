2798689feb7af5af9d45a5c7a48ad78afba36a29
aidankeighron <aidankeighron@gmail.com>
2024-06-05 23:57:44 -0500

    canvas working

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,58 @@
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import css from "./page.module.css";
+import * as THREE from 'three';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+
+function Box(props: ThreeElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+export default function Home() {
+  return(
+    <main className={css.main}>
+      <div className={css.scene}>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
+      </div>
+      <div className={css.header}>
         <p>Aidan Keighron</p>
       </div>
-
-      <div className={styles.timeline}>
-        <div className={styles.card}>
+      <div className={css.timeline}>
+        <div className={css.card}>
           <h3>Alchemy</h3>
           <p>Language: JavaScript</p>
           <p>Tech Stack: Electron and Firebase Firestore</p>
           <p>Personal task organization software</p>
           <a href="https://github.com/aidankeighron/alchemy">GitHub</a>
         </div>
-        <div className={[styles.cardOther, styles.card].join(" ")}>
+        <div className={[css.cardOther, css.card].join(" ")}>
           <h3>PID Visualizer</h3>
           <p>Language: Java</p>
           <p>Tech Stack: Swing</p>
           <p>Visualization of a PID controller</p>
           <a href="https://github.com/aidankeighron/PID-Visualizer">GitHub</a>
         </div>
-        <div className={styles.card}>
+        <div className={css.card}>
           <h3>Fantasy FRC</h3>
           <p>Languages: JavaScript, HTML, and CSS</p>
           <p>Tech Stack: Express and Socket.io</p>
