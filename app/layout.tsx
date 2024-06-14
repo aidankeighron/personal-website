45c2c1bcbbb1e836419e976c4eabaec2734ca8df
aidankeighron <aidankeighron@gmail.com>
2024-06-14 00:32:00 -0500

    vercell

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={inter.className}>{children}</body>
     </html>
   );
