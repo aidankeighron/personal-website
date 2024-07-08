d74ddf71e2c466d4210c99f90e550a9aca502e24
aidankeighron <aidankeighron@gmail.com>
2024-07-08 00:38:54 -0500

    fix code blocks

--- app/components/CodeBlock.tsx
+++ app/components/CodeBlock.tsx
@@ -9,12 +9,13 @@ type CodeBlockParams = {
 
 export default function CodeBlock({code, language}: CodeBlockParams) {
     const [content, setContent] = useState<string>('<></>');
-  
+
     useEffect(() => {
+      console.log(`<pre><code className="${language}">${code}</code></pre>`);
       rehype()
       .data('settings', {fragment: true})
       .use(rehypeStarryNight)
-      .process(`<pre><code className="${language}">${code}</code></pre>`).then((c: any) => {
+      .process(`<pre><code className="${language} codeBlock">${code}</code></pre>`).then((c: any) => {
         console.log(String(c));
         setContent(String(c));
       })
