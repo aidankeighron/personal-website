c28df39f797d10b7430fb9b3742557c34a0d5a78
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-07-31 00:51:16 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Aidan Keighron",
@@ -11,6 +12,7 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
   return (
     <html lang="en" className="dark text-main bg-d-main">
       <SpeedInsights/>
+      <Analytics />
       <body className="bg-main dark:bg-d-main">{children}</body>
     </html>
   );
