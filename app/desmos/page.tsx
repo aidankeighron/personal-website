2ff6ca085973445d801a3582d2cb12125f30c224
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-08-27 12:47:22 -0400

    add back button

--- app/desmos/page.tsx
+++ app/desmos/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
+import Link from "next/link";
+
 const width = '80%'
 const height = 'w-[90%] sm:w-[80%] h-[40vh] sm:h-[50vh] md:h-[80vh]'
 
 export default function Desmos() {
     return (
+        <>
+        <Link href='/'><p className='header-link w-fit fixed z-10 items-end justify-end top-10 left-[2.5%]'>Home</p></Link>
         <div className="flex flex-col px-5 sm:px-24 py-10">
-            <p className="text-6xl mb-5">Desmos</p>
+            <p className="text-6xl mb-5 self-center">Desmos</p>
             <p className="text-lg mb-10">{`Desmos is a web graphing software that I frequently use to quickly graph equations or flesh out geometry.
 My biggest use was physics calculations for a three jointed arm. Here I have added a collection of by most interesting
 graphs, ranging from Bezier curves to physics simulations.`}</p>
@@ -27,5 +31,6 @@ graphs, ranging from Bezier curves to physics simulations.`}</p>
                 <iframe src="https://www.desmos.com/calculator/ow2jgopdrn?embed" width={width} className={height}></iframe>
             </div>
         </div>
+        </>
     )
 }
