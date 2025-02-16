79c93a819a5975d5c0277a54a4e52461f02ecbd7
aidankeighron <aidankeighron@gmail.com>
2025-02-16 02:54:57 -0500

    update about me text

--- app/homeComponents/AboutMe.tsx
+++ app/homeComponents/AboutMe.tsx
@@ -21,11 +21,11 @@ export default function AboutMe() {
             <Image width={4007} height={4004} placeholder='blur' blurDataURL='/images/aidan_profile.webp' priority
                   src={aboutMeImage} alt="Picture of Aidan Keighron" className='h-max w-max rounded-xl' />
             </m.div>
-            <p className='text-d-main font-medium text-sm md:text-lg md:w-2/3 whitespace-pre-line md:mx-4'>{`I am a highly motivated computer science student at Michigan State University, fueled by a passion for robotics and software development.
+            <p className='text-d-main font-medium text-sm md:text-2xl md:w-2/3 whitespace-pre-line md:mx-4'>{`I am a computer science student at Michigan State University.
 
-On the robotics side of things, I co-founded the combat robotics team Bad Conflict, where we build robots to compete in antweight (meaning robots are less than one pound) competitions. My current robot, Horizon, is a horizontal spinner that packs quite a punch.
+My current combat robot, Horizon, is a horizontal spinner that packs quite a punch.
 
-Taking software development to the next level I created a startup called Half Full, where I am making an all-in-one task management software that aims to reduce the time it takes to plan out your week.`}</p>
+I founded a startup called Half Full, where I am making an all-in-one task management software.`}</p>
           </div>
       </div>
     );
